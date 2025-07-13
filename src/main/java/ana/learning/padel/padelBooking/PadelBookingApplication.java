831ef17e92fe2ac7c4ed1bdc1ffeba301958ced0
ANA BOYERO MOLINA <anaboyero@iMac-de-ANA.local>
2025-07-13 12:16:06 +0200

    Primer commmit- proyecto Spring creado

---
+++ src/main/java/ana/learning/padel/padelBooking/PadelBookingApplication.java
@@ -0,0 +1,13 @@
+package ana.learning.padel.padelBooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PadelBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PadelBookingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ana/learning/padel/padelBooking/PadelBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package ana.learning.padel.padelBooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PadelBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
