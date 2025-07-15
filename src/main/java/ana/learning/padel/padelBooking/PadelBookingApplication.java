b2c56e2bd27dceba6787063d9497b08cb2094f13
Ana Boyero <ana.boyero@MacBook-Pro-de-Ana.local>
2025-07-15 09:27:51 +0200

    Add Calendar

--- src/main/java/ana/learning/padel/padelBooking/PadelBookingApplication.java
+++ src/main/java/ana/learning/padel/padelBooking/PadelBookingApplication.java
@@ -1,12 +1,15 @@
 package ana.learning.padel.padelBooking;
 
 import ana.learning.padel.padelBooking.model.Booking;
+import ana.learning.padel.padelBooking.model.BookingCalendar;
 import ana.learning.padel.padelBooking.model.Player;
 import ana.learning.padel.padelBooking.model.Residence;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class PadelBookingApplication {
@@ -20,15 +23,23 @@ public class PadelBookingApplication {
 		Residence residence4 = new Residence(Residence.Building.RAMIREZ_PRADO_7, Residence.Floor.FIFTH_FLOOR, Residence.Letter.D);
 		Residence residence5 = new Residence(Residence.Building.RAMIREZ_PRADO_8, Residence.Floor.SECOND_FLOOR, Residence.Letter.E);
 
-		Player player1 = new Player("Ana Boyero", residence1);
-		Player player2 = new Player("Javier Villuendas", residence4);
+		Player player1 = new Player("Ana", residence1);
+		Player player2 = new Player("Javier", residence4);
 
 		LocalDate today = LocalDate.now();
 		Booking reserva_lunes9AM = new Booking(today, Booking.TimeSlot.NINE_AM, player1);
 		Booking reserva_viernes3PM = new Booking(today.plusDays(3), Booking.TimeSlot.THREE_PM, player2);
 
+		List<Booking> reservations = new ArrayList<>();
+		reservations.add(reserva_lunes9AM);
+		reservations.add(reserva_viernes3PM);
+
 		System.out.println("**** Booking 1: " + reserva_lunes9AM);
 		System.out.println("**** Booking 2: " + reserva_viernes3PM);
+
+//		BookingCalendar calendar = new BookingCalendar();
+//		calendar.setReserved_bookings(reservations);
+//		System.out.println("**** Calendar " + calendar);
 
 	}
 
---
+++ src/main/java/ana/learning/padel/padelBooking/model/BookingCalendar.java
@@ -0,0 +1,63 @@
+package ana.learning.padel.padelBooking.model;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+public class BookingCalendar {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private LocalDate lastUpdate;
+    private List<Booking> available_bookings;
+    @OneToMany
+    @JoinColumn(name = "booking_id")
+    private List<Booking> reserved_bookings;
+
+    public BookingCalendar() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDate getLastUpdate() {
+        return lastUpdate;
+    }
+
+    public void setLastUpdate(LocalDate lastUpdate) {
+        this.lastUpdate = lastUpdate;
+    }
+
+    public List<Booking> getAvailable_bookings() {
+        return available_bookings;
+    }
+
+    public void setAvailable_bookings(List<Booking> available_bookings) {
+        this.available_bookings = available_bookings;
+    }
+
+    public List<Booking> getReserved_bookings() {
+        return reserved_bookings;
+    }
+
+    public void setReserved_bookings(List<Booking> reserved_bookings) {
+        this.reserved_bookings = reserved_bookings;
+    }
+
+    @Override
+    public String toString() {
+        return "BookingCalendar{" +
+                "id=" + id +
+                ", lastUpdate=" + lastUpdate +
+                ", available_bookings=" + available_bookings +
+                ", reserved_bookings=" + reserved_bookings +
+                '}';
+    }
+}
