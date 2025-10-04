c36dcbf4d86ae413ac78e1f11401efa4fd36b599
X47517BO <ana.boyero@thoughtworks.com>
2025-10-04 12:58:11 +0200

    Change return type of deleteAllBookingCalendars to BookingCalendarDTO

--- src/main/java/ana/learning/padel/padelBooking/controller/BookingCalendarController.java
+++ src/main/java/ana/learning/padel/padelBooking/controller/BookingCalendarController.java
@@ -89,9 +89,8 @@ public class BookingCalendarController {
 //    }
 
     @DeleteMapping
-    public ResponseEntity<BookingCalendar> deleteAllBookingCalendars() {
+    public ResponseEntity<BookingCalendarDTO> deleteAllBookingCalendars() {
         bookingCalendarService.deleteAllBookingCalendars();
         return ResponseEntity.noContent().build();
     }
-
 }
